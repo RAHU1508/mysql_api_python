ebf5e58547c7bdd9d465df0aed86e6a6edef0c57
RAHU1508 <rahulxyz96@gmail.com>
2022-08-21 16:35:23 +0530

    communication between mongodb, API(Postman) & Python

---
+++ mongodb.py
@@ -0,0 +1,24 @@
+from flask import Flask,request, jsonify
+import pymongo
+app = Flask(__name__)
+
+client = pymongo.MongoClient("mongodb+srv://RAHU1508:mongobd123@rahu1508.g93ki.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
+database = client['taskdb']
+collection= database['taskcollection']
+
+@app.route('/insert/mongo', methods=['POST', 'GET'])
+def insert():
+    if request.method=='POST':
+        name = request.json['name']
+        number = request.json['number']
+        collection.insert_one({name:number})
+        return jsonify(str('successfully inserted'))
+
+
+if __name__ == '__main__':
+    app.run(port = 5001)
+
+
+
